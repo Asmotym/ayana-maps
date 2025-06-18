f9338b64834cc36076b17e096720f9bcf46ff756
Asmotym <clement.boucherpro@gmail.com>
2025-06-19 01:45:17 +0200

    Update global code

--- netlify/core/database/index.ts
+++ netlify/core/database/index.ts
@@ -1,6 +1,6 @@
 import { neon } from '@netlify/neon';
 
-export const sql = neon(process.env.NEON_DATABASE_URL);
+export const sql = neon(process.env.NETLIFY_DATABASE_URL);
 
 // export queries handler
 export * from './queries-handler.core';
